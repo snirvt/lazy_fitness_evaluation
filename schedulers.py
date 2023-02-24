15e9b8ae82688912d83bf9a81d1b9e952a8c39b4
snirvt <snirvt@gmail.com>
2023-02-24 04:06:43 +0200

    fixed linear scheduler

--- schedulers.py
+++ schedulers.py
@@ -1,20 +1,19 @@
 
 
 
-'''The step_scheduler is a step function, where it calculates the ratio of fitness calls and then check what
-fitness percentage is best suited.
-The steps parameter is a list of tuples where each tuple represent a cumulative proportion (first value) and a
-corresponding fitness percentage (last value).
-The default is p=1
-When the ratio of fitness calls surpassed the cumulative proportions it will return 1.
 '''
-def step_scheduler(f_call, n_fitness_calls, steps = [(0.2,0.5),(0.2,0.75)]):
-    ratio = f_call/n_fitness_calls
-    sum_p = 0
-    for step in steps:
-        sum_p += step[0]
-        if ratio <= sum_p:
-            return step[1]
+The step_scheduler is a step function, where it calculates the intermediate values for the ratio of fitness calls
+according to the step points.
+if the points are [(0,0.5), (0.2,0.7), (1,1)], all ratios below 0.2 will return 0.5, else - all the ratios below 1
+will return 0.7, and then ratio equal to 1 will return 1
+'''
+
+def step_scheduler(f_call, n_fitness_calls, points):
+    points += [(1, 1)]
+    ratio = f_call / n_fitness_calls
+    for i in range(len(points)-1):
+        if ratio < points[i+1][0]:
+            return points[i][1]
     return 1
 
 
@@ -46,3 +45,10 @@ if __name__ == "__main__":
     print(linear_scheduler(1, 100, points))
     print(linear_scheduler(20, 100, points))
     print(linear_scheduler(45, 100, points))
+
+    print(step_scheduler(0, 100, points))
+    print(step_scheduler(21, 100, points))
+    print(step_scheduler(65, 100, points))
+
+
+
