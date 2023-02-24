6bed6c2000ecc4bf2b66230274887c06b0c50825
snirvt <snirvt@gmail.com>
2023-02-24 04:34:32 +0200

    divide now the distance for lazy tsp by p

--- fitness_handler.py
+++ fitness_handler.py
@@ -8,11 +8,11 @@ def evalTSP(individual, distances):
     return distance,
 
 
-def evalTSP_lazy(individual, distances, p=0.5):
+def evalTSP_lazy(individual, distances, p=1):
     start_idx = random.sample(range(len(individual)), 1)[0]
     end_idx = start_idx + int(len(individual) * p)
     double_individual = list(individual) + list(individual)
     distance = 0
     for gene1, gene2 in zip(double_individual[start_idx:end_idx], double_individual[start_idx+1:end_idx+1]):
         distance += distances[gene1][gene2]
-    return distance,
+    return distance/p,
