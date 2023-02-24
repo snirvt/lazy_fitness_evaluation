e52976545df381502b9334e9bf5e29a54ef46b85
snirvt <snirvt@gmail.com>
2023-02-24 10:23:53 +0200

    clear hall of fame when p has changed

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ warnings.filterwarnings("ignore")
 
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--INDIVIDUAL_SIZE', type=int, default=20, help='')#, required=False)
+argparser.add_argument('--INDIVIDUAL_SIZE', type=int, default=20, help='')
 argparser.add_argument('--POPULATION_SIZE', type=int, default=100, help='')
 argparser.add_argument('--cxpb', type=float, default=0.1, help='cross over percentage')
 argparser.add_argument('--mutpb', type=float, default=1.0, help='mutation percentage')
@@ -30,6 +30,9 @@ argparser.add_argument('--n_fitness_calls', type=int, default=100000, help='max 
 argparser.add_argument('--novelty_param', type=int, default=1, help='')
 argparser.add_argument('--scheduler', choices=['step_scheduler', 'linear_scheduler'], default=step_scheduler, help='')
 argparser.add_argument('--points', type=list, default=[(0, 0.8),(0.2, 1)], help='scheduler function argument')
+argparser.add_argument('--verbos', type=bool, default=False, help='scheduler function argument')
+
+# argparser.add_argument('--points', type=list, default=[(0, 1),(1, 1)], help='scheduler function argument')
 
 args, unknown = argparser.parse_known_args()
 
@@ -77,10 +80,9 @@ toolbox.register("evaluate", evalTSP_lazy, distances=distances, p=1)
 
 
 pop = toolbox.population(n=POPULATION_SIZE)
-
-# p = 1
 f_call = 0
 p = scheduler(f_call, n_fitness_calls, points)
+prev_p = p
 hof = tools.HallOfFame(novelty_param)
 toolbox.register("evaluate", evalTSP_lazy, distances=distances, p=p)
 fitnesses = list(map(toolbox.evaluate, pop))
@@ -91,17 +93,22 @@ hof.update(pop)
 fits = [ind.fitness.values[0] for ind in pop]
 g = 0
 log = []
+
 while f_call < n_fitness_calls:
     g = g + 1
-    print(f"-- Generation {g}, p {p}--")
+    if args.verbos:
+        print(f"-- Generation {g}, p {p}--")
     offspring = toolbox.select(pop, len(pop))
     offspring = list(map(toolbox.clone, offspring))
     hof.update(offspring)
     xover(offspring, cxpb, toolbox)
     mutation(offspring, mutpb, toolbox)
-    if novelty_param > 0:
+    if novelty_param > 0 and len(hof) > 0:
         pop[:novelty_param] = hof.items[:]
     p = scheduler(f_call, n_fitness_calls, points)
+    if p != prev_p:
+        hof.clear()
+    prev_p = p
     invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
     toolbox.register("evaluate", evalTSP_lazy, distances=distances, p=p)
     fitnesses = map(toolbox.evaluate, invalid_ind)
@@ -111,20 +118,13 @@ while f_call < n_fitness_calls:
             break
         ind.fitness.values = fit
     pop[:] = offspring
-    if novelty_param > 0:
+    if novelty_param > 0 and len(hof) > 0:
         pop[:novelty_param] = hof.items[:]
+
     valid_ind = [ind for ind in offspring if ind.fitness.valid]
     fits = [ind.fitness.values[0] for ind in valid_ind]
-    gen_stats = {'mean': np.mean(fits), 'std': np.std(fits), 'min': min(fits), 'max': max(fits)}
+    gen_stats = {'mean': np.mean(fits), 'std': np.std(fits), 'min': min(fits), 'max': max(fits), 'p': p}
     log.append(gen_stats)
 
-
 best_ind = tools.selBest(pop, 1)[0]
 print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
-toolbox.register("evaluate", evalTSP, distances=distances)
-fitnesses = list(map(toolbox.evaluate, pop))
-for ind, fit in zip(pop, fitnesses):
-    ind.fitness.values = fit
-best_ind = tools.selBest(pop, 1)[0]
-print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
-
