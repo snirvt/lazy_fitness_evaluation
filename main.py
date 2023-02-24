08eae1a95c7e498b5b781b1e1924258b0eb9bd47
snirvt <snirvt@gmail.com>
2023-02-24 06:12:24 +0200

    argparser added

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+import sys
+print(sys.prefix)
+import os.path
+
 import random
 import numpy as np
 from deap import algorithms, base, creator, tools
@@ -7,16 +12,47 @@ import random
 
 from utils import generate_distances
 from fitness_handler import evalTSP, evalTSP_lazy
+from schedulers import step_scheduler, linear_scheduler
+import argparse
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--INDIVIDUAL_SIZE', type=int, default=20, help='')#, required=False)
+argparser.add_argument('--POPULATION_SIZE', type=int, default=100, help='')
+argparser.add_argument('--cxpb', type=float, default=0.1, help='cross over percentage')
+argparser.add_argument('--mutpb', type=float, default=1.0, help='mutation percentage')
+argparser.add_argument('--n_fitness_calls', type=int, default=100000, help='max fitness calls')
+argparser.add_argument('--novelty_param', type=int, default=1, help='')
+argparser.add_argument('--scheduler', choices=['step_scheduler', 'linear_scheduler'], default=step_scheduler, help='')
+argparser.add_argument('--points', type=list, default=[(0, 0.8),(0.2, 1)], help='scheduler function argument')
+
+args, unknown = argparser.parse_known_args()
+
+
+
+
+
+
+
 
 # Define problem parameters
-NUM_CITIES = 20  # Number of cities in the TSP problem
-INDIVIDUAL_SIZE = NUM_CITIES  # Number of genes in each individual
-POPULATION_SIZE = 100  # Number of individuals in the population
-cxpb = 0.1  # Probability of performing crossover on an individual
-mutpb = 0.75  # Probability of mutating an individual
-ngen = 100  # Number of generations to evolve the population
-n_fitness_calls = 100000
+INDIVIDUAL_SIZE = args.INDIVIDUAL_SIZE  # Number of genes in each individual
+NUM_CITIES = INDIVIDUAL_SIZE  # Number of cities in the TSP problem
+POPULATION_SIZE = args.POPULATION_SIZE  # Number of individuals in the population
+cxpb = args.cxpb  # Probability of performing crossover on an individual
+mutpb = args.mutpb  # Probability of mutating an individual
+n_fitness_calls = args.n_fitness_calls
+novelty_param = args.novelty_param
 
+scheduler = step_scheduler
+if args.scheduler == 'linear_scheduler':
+    scheduler = linear_scheduler
+
+points = args.points
 
 distances = generate_distances(NUM_CITIES)
 
@@ -39,10 +75,11 @@ toolbox.register("evaluate", evalTSP_lazy, distances=distances, p=1)
 
 pop = toolbox.population(n=POPULATION_SIZE)
 
-p = 1
+# p = 1
 f_call = 0
-hof = tools.HallOfFame(1)
-
+p = scheduler(f_call, n_fitness_calls, points)
+hof = tools.HallOfFame(novelty_param)
+toolbox.register("evaluate", evalTSP_lazy, distances=distances, p=p)
 fitnesses = list(map(toolbox.evaluate, pop))
 for ind, fit in zip(pop, fitnesses):
     f_call += p
@@ -58,23 +95,13 @@ g = 0
 log = []
 # while g < ngen:
 while f_call < n_fitness_calls:
-    # A new generation
     g = g + 1
-    print("-- Generation %i --" % g)
-
-    # Select the next generation individuals
     offspring = toolbox.select(pop, len(pop))
-    # Clone the selected individuals
     offspring = list(map(toolbox.clone, offspring))
     hof.update(offspring)
-
-    # Apply crossover and mutation on the offspring
-    for child1, child2 in zip(offspring[::2], offspring[1::2]):
-
-        # cross two individuals with probability CXPB
-        if random.random() < cxpb:
-            toolbox.mate(child1, child2)
-            # fitness values of the children must be recalculated later
+    for child1, child2 in zip(offspring[::2], offspring[1::2]): # Apply crossover and mutation on the offspring
+        if random.random() < cxpb:# cross two individuals with probability CXPB
+            toolbox.mate(child1, child2) # fitness values of the children must be recalculated later
             del child1.fitness.values
             del child2.fitness.values
 
@@ -84,7 +111,12 @@ while f_call < n_fitness_calls:
             toolbox.mutate(mutant)
             del mutant.fitness.values
     # Evaluate the individuals with an invalid fitness
+
+    if novelty_param > 0:
+        pop[:novelty_param] = hof.items[:]
+    p = scheduler(f_call, n_fitness_calls, points)
     invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    toolbox.register("evaluate", evalTSP_lazy, distances=distances, p=p)
     fitnesses = map(toolbox.evaluate, invalid_ind)
     for ind, fit in zip(invalid_ind, fitnesses):
         f_call += p
@@ -94,14 +126,20 @@ while f_call < n_fitness_calls:
     print("  Evaluated %i individuals" % len(invalid_ind))
     # The population is entirely replaced by the offspring
     pop[:] = offspring
+    if novelty_param > 0:
+        pop[:novelty_param] = hof.items[:]
     # Gather all the fitnesses in one list and print the stats
     valid_ind = [ind for ind in offspring if ind.fitness.valid]
     fits = [ind.fitness.values[0] for ind in valid_ind]
     gen_stats = {'mean': np.mean(fits), 'std': np.std(fits), 'min': min(fits), 'max': max(fits)}
     log.append(gen_stats)
 
+
+
+toolbox.register("evaluate", evalTSP, distances=distances)
+fitnesses = list(map(toolbox.evaluate, pop))
+for ind, fit in zip(pop, fitnesses):
+    ind.fitness.values = fit
 best_ind = tools.selBest(pop, 1)[0]
 print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
-
-
 
